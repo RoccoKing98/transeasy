8f1921705e3e0f109e433e4a1ac13fc4189b687e
Rocco King <primal.rots_0c@icloud.com>
2023-08-20 18:43:29 +0200

    Minor improvements to tests

--- spec/unit/models/translation_settings_spec.rb
+++ spec/unit/models/translation_settings_spec.rb
@@ -10,19 +10,20 @@ module Transeasy
       it 'requires a root language' do
         translation_settings.root_language = nil
         translation_settings.valid?
-        expect(translation_settings.errors[:root_language]).to be_present
+        expect(translation_settings.errors[:root_language]).to be == ["can't be blank"]
       end
 
       it 'requires at least one target language' do
         translation_settings.target_languages = nil
         translation_settings.valid?
-        expect(translation_settings.errors[:target_languages]).to be_present
+        expect(translation_settings.errors[:target_languages]).to be == ["can't be blank",
+                                                                         'Invalid class NilClass for target languages']
       end
 
       it 'requires that the root language cannot be a target language' do
         translation_settings.target_languages = [translation_settings.root_language].to_json
         translation_settings.valid?
-        expect(translation_settings.errors[:root_language]).to be_present
+        expect(translation_settings.errors[:root_language]).to be == ['Root language cannot also be a target language']
       end
     end
   end
