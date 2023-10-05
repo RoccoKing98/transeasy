ba4ae23e76f9ddbfdf6ff1fb6a8ee62785e436c2
Rocco King <primal.rots_0c@icloud.com>
2023-10-05 15:24:55 +0200

    Keep RuboCop happy

--- spec/unit/models/translation_settings_spec.rb
+++ spec/unit/models/translation_settings_spec.rb
@@ -6,25 +6,23 @@ module Transeasy
   RSpec.describe TranslationSetting, type: :model do
     let(:translation_settings) { FactoryBot.create(:translation_setting) }
 
-    context 'validations' do
-      it 'requires a root language' do
-        translation_settings.root_language = nil
-        translation_settings.valid?
-        expect(translation_settings.errors[:root_language]).to be == ["can't be blank"]
-      end
+    it 'setup requires a root language' do
+      translation_settings.root_language = nil
+      translation_settings.valid?
+      expect(translation_settings.errors[:root_language]).to be == ["can't be blank"]
+    end
 
-      it 'requires at least one target language' do
-        translation_settings.target_languages = nil
-        translation_settings.valid?
-        expect(translation_settings.errors[:target_languages]).to be == ["can't be blank",
-                                                                         'Invalid class NilClass for target languages']
-      end
+    it 'setup requires at least one target language' do
+      translation_settings.target_languages = nil
+      translation_settings.valid?
+      expect(translation_settings.errors[:target_languages]).to be == ["can't be blank",
+                                                                       'Invalid class NilClass for target languages']
+    end
 
-      it 'requires that the root language cannot be a target language' do
-        translation_settings.target_languages = [translation_settings.root_language].to_json
-        translation_settings.valid?
-        expect(translation_settings.errors[:root_language]).to be == ['Root language cannot also be a target language']
-      end
+    it 'setup requires that the root language cannot be a target language' do
+      translation_settings.target_languages = [translation_settings.root_language].to_json
+      translation_settings.valid?
+      expect(translation_settings.errors[:root_language]).to be == ['Root language cannot also be a target language']
     end
   end
 end
